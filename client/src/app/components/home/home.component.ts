cc38012d535edd535d9f757b0766a7299aa5bcb6
Denis Ouma <dennisouma38@gmail.com>
2023-03-02 12:35:47 +0300

    bug fix

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { ContactService } from 'src/app/services/contact.service';
+import { AuthService } from 'src/app/services/auth.service';
+
+import { User } from 'src/app/models/User';
+import { Contact } from 'src/app/models/Contact';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: [],
 })
 export class HomeComponent {
-  createPost(): void {
-    console.log('Hello');
+  contact$: Observable<Contact[]> | undefined = undefined;
+  userId: number | undefined;
+  constructor(
+    private contactService: ContactService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.contact$ = this.fetchContacts();
+    this.userId = this.authService.userId?.id;
+  }
+
+  fetchContacts(): Observable<Contact[]> {
+    return this.contactService.fetchContacts();
+  }
+  createContact(): void {
+    this.contact$ = this.fetchContacts();
+  }
+  deleteContact(contactId: Pick<Contact, 'id'>): void {
+    this.contactService
+      .deleteContact(contactId)
+      .subscribe(() => (this.contact$ = this.fetchContacts()));
   }
 }
--- client/src/app/services/contact.service.ts
+++ client/src/app/services/contact.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Contact } from '../models/Contact';
+import { User } from '../models/User';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { first, catchError } from 'rxjs/operators';
+import { ErrorHandlerService } from './error-handler.service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
+  private url = 'http://localhost:3000/api';
 
-  constructor() { }
+  httpOptions: { headers: HttpHeaders } = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(
+    private http: HttpClient,
+    private errorHandlerService: ErrorHandlerService
+  ) {}
+
+  fetchContacts(): Observable<Contact[]> {
+    return this.http
+      .get<Contact[]>(`${this.url}/contacts`)
+      .pipe(
+        catchError(
+          this.errorHandlerService.handleError<Contact[]>('fetchAll', [])
+        )
+      );
+  }
+  createContact(
+    formData: Partial<Contact>,
+    userId: Pick<User, 'id'>
+  ): Observable<Contact> {
+    return this.http
+      .post<Contact>(
+        `${this.url}/contacts`,
+        {
+          firstName: formData.firstName,
+          lastName: formData.lastName,
+          email: formData.email,
+          phone: formData.phone,
+          type: formData.type,
+          user: userId,
+        },
+        this.httpOptions
+      )
+      .pipe(
+        catchError(
+          this.errorHandlerService.handleError<Contact>('createContact')
+        )
+      );
+  }
+
+  deleteContact(contactId: Pick<Contact, 'id'>): Observable<{}> {
+    return this.http
+      .delete<Contact>(`${this.url}/${contactId}`, this.httpOptions)
+      .pipe(
+        first(),
+        catchError(
+          this.errorHandlerService.handleError<Contact>('deleteContact')
+        )
+      );
+  }
 }
