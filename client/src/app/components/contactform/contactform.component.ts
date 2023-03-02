7211a058074533fcbe2a06222eb9f9255dc5c573
Denis Ouma <dennisouma38@gmail.com>
2023-03-02 10:51:24 +0300

    Updating the form on submit

--- client/src/app/components/contactform/contactform.component.ts
+++ client/src/app/components/contactform/contactform.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { NgForm } from '@angular/forms';
+import { Contact } from 'src/app/models/Contact';
 @Component({
   selector: 'app-contactform',
   templateUrl: './contactform.component.html',
   styles: [],
 })
 export class ContactformComponent {
+  @ViewChild('formDirective') formDirective!: NgForm;
   form: FormGroup;
 
   constructor() {
@@ -32,7 +34,10 @@ export class ContactformComponent {
       type: new FormControl('', [Validators.required, Validators.minLength(3)]),
     });
   }
-  onSubmit(): void {
-    console.log('contact');
+  onSubmit(
+    formData: Pick<Contact, 'firstName' | 'lastName' | 'email' | 'phone'>
+  ): void {
+    console.log(formData);
+    this.formDirective.resetForm();
   }
 }
